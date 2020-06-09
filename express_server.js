5606dd5fb677a6e048aed7e281cbbeeadf638f44
Kevin Kim <kevinfkim@gmail.com>
2020-06-09 01:55:53 +0000

    3 edge cases added: non-existent shortURL, when server restarted urlDatabse is saved, and error handling... kinda.

--- express_server.js
+++ express_server.js
@@ -2,26 +2,29 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const fs = require("fs");
+let urlDatabase = require("./data/urlDatabase.json");
 
 // set ejs as the view engine, and use body-parser to parse POST body from Buffer
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// url data we will work with
-let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
+// helper function
 function generateRandomString() {
   let ranChars = Math.random().toString(36).substr(2, 6);
   return ranChars;
 }
 
-
 // server to listen for client requests
 app.listen(PORT, () => {
   console.log(`Example app, listening on port ${PORT}`);
+});
+
+// error handling...kinda
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send('Something broke!');
+  next();
 });
 
 // setup the server with a welcome msg on a GET request for "/"
@@ -34,13 +37,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b> World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  let theURL = { urls: urlDatabase };
+  res.render("urls_index", theURL);
 });
 
 app.get("/urls/new", (req, res) => {
@@ -49,22 +48,29 @@ app.get("/urls/new", (req, res) => {
 
 // ":xxxx is to signify variable deposits"
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  if (templateVars.longURL === undefined) {
-    res.send("<html><body><b>404 ERROR</b><br>Long URL for " + templateVars.shortURL + " doesn't exist.<br>Please try again.</body></html>\n");
+  let theURL = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  if (theURL.longURL === undefined) {
+    res.send("<html><body><b>404 ERROR</b><br>Long URL for " + theURL.shortURL + " doesn't exist.<br>Please try again.</body></html>\n");
   } else {
-    // console.log(req.params);
-    // console.log(templateVars);
-    res.render("urls_show", templateVars);
+    res.render("urls_show", theURL);
   }
 });
 
 // create and redirect to new shortURL from form
+// add new url key/value pair to json file
 app.post("/urls", (req, res) => {
-  console.log(req.body);
+
   const generatedShortURL = generateRandomString();
-  urlDatabase[generatedShortURL] = req.body.longURL;
-  let templateVars = { shortURL: generatedShortURL, longURL: urlDatabase[generatedShortURL] };
+  let theURL = { shortURL: generatedShortURL, longURL: urlDatabase[generatedShortURL] };
+
+  let newURL = {};
+  newURL[theURL.shortURL] = req.body.longURL;
+  Object.assign(urlDatabase, newURL);
+
+  fs.writeFile("./data/urlDatabase.json", JSON.stringify(urlDatabase), (err) => {
+    if (err) throw err;
+  });
+
   res.redirect(`/urls/${generatedShortURL}`);
 });
 
@@ -74,8 +80,3 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL.longURL);
 });
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-  next();
-});
